56cba665836c1928ba2ebc0bde4e6bdd47747342
sozdam3d <sozdam3d@gmail.com>
2023-06-06 18:48:51 +0300

    READY

--- Final_Task/wow.py
+++ Final_Task/wow.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def game_core_v3(number: int = 1) -> int:
     """
     Args:
@@ -7,33 +9,43 @@ def game_core_v3(number: int = 1) -> int:
         int: Число попыток
     """
     # Ваш код начинается здесь
-    import numpy as np
+    x,y=1,101 
+    count=0
 
-    def function_knife(number,predict,count=None,x=None,y=None):
-        if (x==None and y==None):
-            x,y=1,101
-        
-        if count==None:
-          count=1
-        else:
-          count+=1
-        predict=np.random.randint(x,y)
+    while True:
+        predict = (np.random.randint(x, y))
+        count+=1
         if predict==number:
-            return count
-        elif predict > number:
-            return function_knife(number,predict,count,x,y=predict+1)
-        elif predict < number:
-            return function_knife(number,predict,count,y,x=predict)
-    
-    predict = (np.random.randint(1, 101))
-    count=function_knife(number,predict,count=None,x=None,y=None)
-    print(count)
-    
-
+            break
+        elif predict>number:
+            y=predict
+        elif predict<number:
+            x=predict + 1
+             
     # Ваш код заканчивается здесь
-
     return count
-#---------------------
-asd=game_core_v3()
-print('По итогу count = ',asd)
 
+
+
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(10000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
+print('Run benchmarking for game_core_v3: ', end='')
+if __name__ == '__main__':
+    score_game(game_core_v3)
